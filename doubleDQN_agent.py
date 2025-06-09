5b4f3030c7a17ccc35a5dea32d585e44e0e79948
chenzhigang00 <1312383034@qq.com>
2025-06-09 19:51:01 +0800

    annotation

---
+++ doubleDQN_agent.py
@@ -0,0 +1,76 @@
+import numpy as np
+import random
+from collections import namedtuple, deque
+
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+
+from experience_replay import ReplayBuffer
+from qnet import QNetwork
+
+LR = 5e-4                
+BUFFER_SIZE = int(1e5)   
+BATCH_SIZE = 64          
+GAMMA = 0.99             
+TAU = 1e-3               # 目标网络参数软更新
+epsilon = epsilon_start = 1.0
+epsilon_end = 0.01
+epsilon_decay = 0.995
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class Agent():
+    def __init__(self, state_size, action_size): 
+        self.state_size = state_size
+        self.action_size = action_size
+        # DDQN: 建模两个独立的 Q函数神经网络，在动作神经网络上计算最大动作，但在目标神经网络上取值，目的是减小 Q函数过估计问题
+        # 原来 DQN是动作选择及取值都在目标网络上，造成了 Q值过估计
+        self.qnetwork_local = QNetwork(state_size, action_size).to(device)
+        self.qnetwork_target = QNetwork(state_size, action_size).to(device)
+        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
+        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE)   # 经验重放池
+        
+    def step(self, state, action, reward, next_state, done):
+        self.memory.add(state, action, reward, next_state, done)        
+        if len(self.memory) > BATCH_SIZE:
+            experiences = self.memory.sample()   # 采样
+            self.learn(experiences, GAMMA)
+                
+    def act(self, state):
+        global epsilon
+        epsilon = max(epsilon_end, epsilon_decay * epsilon)
+        eps = epsilon
+        state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+        self.qnetwork_local.eval()
+        with torch.no_grad():
+            action_values = self.qnetwork_local(state)
+        self.qnetwork_local.train()
+
+        if random.random() > eps:
+            return np.argmax(action_values.cpu().data.numpy())
+        else:
+            return random.choice(np.arange(self.action_size))
+        
+    def learn(self, experiences, gamma):
+        states, actions, rewards, next_states, dones = experiences
+        self.qnetwork_local.eval()
+        with torch.no_grad():
+            # DDQN
+            Q_pred = self.qnetwork_local(next_states)   # 在动作网络上计算最大动作
+            max_actions = torch.argmax(Q_pred, dim=1).long().unsqueeze(1)    
+            Q_next = self.qnetwork_target(next_states)  # 在目标网络上取值
+        self.qnetwork_local.train()
+        Q_targets = rewards + (gamma * Q_next.gather(1, max_actions) * (1.0 - dones))
+        Q_expected = self.qnetwork_local(states).gather(1, actions)        
+
+        loss = F.mse_loss(Q_expected, Q_targets)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)   # 对目标网络软更新
+        
+    def soft_update(self, local_model, target_model, tau):
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)   # 目标网络参数产生变化，而非直接替代
+        
--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 # Import the Necessary Packages
-import gym
+import gymnasium as gym
 import random
 import torch
 import numpy as np
 from collections import deque
 import matplotlib.pyplot as plt
 
-from doubleDuelingDQN_agent import Agent
+from doubleDQN_agent import Agent
 from qnet import QNetwork
 
-from pyvirtualdisplay import Display
+def seed_everything(seed=42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
-env = gym.make('LunarLander-v2')
-env.seed(0);
+env = gym.make("LunarLander-v3", continuous = False, gravity = -10.0,
+               enable_wind = False, wind_power=15.0, turbulence_power=1.5, render_mode=None)  # LunarLander-v3 with Gymnasium
+seed_everything()
+env.reset(seed=42)
+state_dim = env.observation_space.shape[0]
+action_dim = env.action_space.n
+agent = Agent(state_size=state_dim, action_size=action_dim)
 
-agent = Agent(state_size=8, action_size=4, seed=0)
-
-
-# load the weights from file
 agent.qnetwork_local.load_state_dict(torch.load('checkpoint.pth'))
 
 for i in range(5):
